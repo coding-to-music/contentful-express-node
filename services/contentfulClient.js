49e9e92c0eeae6168a69c840d4c7878d9bbaac8c
coding-to-music <connors.tom@gmail.com>
2022-05-01 16:27:39 +0000

    Build

--- services/contentfulClient.js
+++ services/contentfulClient.js
@@ -2,7 +2,7 @@ var contentful = require("contentful");
 require("dotenv").config();
 
 var client = contentful.createClient({
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  accessToken: process.env.CONTENTFUL_CONTENT_DELIVERY_API,
   space: process.env.CONTENTFUL_SPACE_ID,
 });
 
